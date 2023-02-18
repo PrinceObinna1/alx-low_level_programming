c7855ac51c400bde2ca902c3040e1acfb411afe3
PrinceObinna1 <obcreatours@gmail.com>
2023-02-17 16:58:01 -0800

    Complete the source code in order to print the last digit of the number stored in the variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,9 @@
 #include <time.h>
 
 /**
- * main - print last digit
- * and whether it is greater than 5, less than 6, or 0
+ * main - print the last digit of a randomly generated number
+ * and whether it is greater than 5, less than 6, or 0.
+ *
  * Return: Always 0
  */
 int main(void)
