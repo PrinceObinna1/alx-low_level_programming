a3444d626a477cac911ed55b47ef8ca8bbcaedfb
PrinceObinna1 <obcreatours@gmail.com>
2023-02-17 18:17:58 -0800

    Complete the source code in order to print the last digit of the number stored in the variable

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
+/* more headers goes there */
 
+/* betty style doc for function main goes there */
 /**
  * main - Entry
+ *
  * Return: Always 0
  */
 int main(void)
