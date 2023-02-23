9ab6c0107a77c91a0ee1718f3be4d908a73540ae
PrinceObinna1 <obcreatours@gmail.com>
2023-02-23 14:05:28 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,9 +20,12 @@ void times_table(void)
 
 			if (prod <= 9)
 				_putchar(' ');
-			else
+
+			if (prod == 0)
+			{
 				_putchar((prod / 10) + '0');
 				_putchar((prod % 10) + '0');
+			}
 		}
 		_putchar('\n');
 	}
