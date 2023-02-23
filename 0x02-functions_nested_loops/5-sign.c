c8e78c050ca8622d929a5af172d5ca5791e3be9d
PrinceObinna1 <obcreatours@gmail.com>
2023-02-22 21:22:13 -0800

    sign of a number

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,9 @@
 /**
  * print_sign - prints sign based on condition
  * @n: argument passed
- * Return: 1, 0 -1
+ * Return: 1 and prints + if n is greater than zero
+ * * 0 and prints 0 if n is zero
+ * * -1 and prints - if n is less than zero
  */
 int print_sign(int n)
 {
@@ -19,7 +21,7 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-		return (-1);
 	}
-}
+		return (-1);
 
+}
