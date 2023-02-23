4d881a7d9d7c99341905b84da295bc9ed7c5d964
PrinceObinna1 <obcreatours@gmail.com>
2023-02-23 13:57:29 -0800

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,32 +5,25 @@
 
 void times_table(void)
 {
-	int i, j, k;
+	int num, mult, prod;
 
-	for (i = 0; i < 10; i++)
+	for (num = 0; num <= 9; num++)
 	{
-	for (j = 0; j < 10; j++)
-	{
-		k = j * i;
+		_putchar('0');
 
-		if (j == 0)
-		{
-			_putchar(k + '0');
-		}
-
-		if (k < 10 && j != 0)
+		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putchar(k + '0');
-		}
+			_putchar(' ');
 
-		else (k > = 10);
-		{
-			_putchar(',');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+				_putchar((prod % 10) + '0');
 		}
-	}
-	_putchar('\n');
+		_putchar('\n');
 	}
 }
